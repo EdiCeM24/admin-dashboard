8084e7041d8459ec0c496be911d3e6000261a6cd
Marcus Edidiong Cletus <edi_mars@outlook.com>
2023-01-13 13:03:57 -0800

    install react js and create template

---
+++ src/data/mockData.js
@@ -0,0 +1,34 @@
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: 'Meak Snow',
+        email: 'meaksnow234@gmail.com',
+        age: 34,
+        phone: '(+234)8034267429',
+        access: 'admin',
+    },
+    {
+        id: 2,
+        name: 'Hellin Paul',
+        email: 'Felix Brauns@gmail.com',
+        phone: '(+234)8042679012',
+        age: 38,
+        access: 'manager',
+    },
+    {
+        id: 3,
+        name: 'Martins Hawklans',
+        email: 'manhawks@yahoo.com',
+        phone: '(+234)8093271635',
+        age: 42,
+        access: 'store manager',
+    },
+    {
+        id: 4,
+        name: 'Rose Helixen',
+        email: 'rosesunshine24@yahoo.com',
+        phone: '(+234)8059382674',
+        age: 30,
+        access: 'project manager',
+    },
+]
